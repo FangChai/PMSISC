a30cdc257b5dbf6d90f2a4c346318d6852ee05c8
Sky Liu <blackskygg@gmail.com>
2015-11-03 19:22:03 +0800

    complete collector

--- csismp_collector.cpp
+++ csismp_collector.cpp
@@ -14,6 +14,7 @@ extern "C" {
 #include <vector>
 #include <mutex>
 #include <string>
+#include <algorithm>
 #include "csismp_limits.h"
 #include "session.h"
 #include "timer.h"
@@ -27,12 +28,12 @@ enum tlv_type {
         TLV_ID,
         TLV_NAME,
         TLV_FACULTY
-}
+};
 
 using namespace std;
 
 struct control_code {
-        uint8_t type;
+        session_type type;
         uint8_t begin;
         uint8_t end;
         uint32_t slice_nr;
@@ -42,11 +43,7 @@ struct control_code {
 struct tlv {
         tlv_type type;
         uint8_t len;
-        union v {
-                string id;
-                string name;
-                string faculty;
-        };
+        string data;
 };
 
 struct slice {
@@ -72,20 +69,9 @@ static struct mac_configure configure;
 static pthread_mutex_t collector_mtx;
 
 
-//get control code, and adjust the byte order
-void parse_control(struct control_code* ctrl, const uint8_t* raw)
+bool cmp_slice(const struct slice& a, const struct slice& b)
 {
-        ctrl->type = raw[0];
-        ctrl->begin = raw[1] & 0x80 ? 1 : 0;
-        ctrl->end = raw[1] & 0x40 ? 1 : 0;
-
-        ctrl->slice_nr = 0;
-        ctrl->slice_nr = *((uint32_t *) (raw));
-        ctrl->slice_nr = ctrl->slice_nr & htobe32(0x003fffff);
-        ctrl->slice_nr = be32toh(ctrl->slice_nr);
-
-        ctrl->id = *((uint32_t *) (raw + 4));
-        ctrl->id = be32toh(ctrl->id);
+        return a.slice_nr < b.slice_nr;
 }
 
 int cmp_mac(const uint8_t mac1[], const uint8_t mac2[])
@@ -101,6 +87,22 @@ int cmp_mac(const uint8_t mac1[], const uint8_t mac2[])
         return result;
 }
 
+//get control code, and adjust the byte order
+void parse_control(struct control_code* ctrl, const uint8_t* raw)
+{
+        ctrl->type = (session_type)raw[0];
+        ctrl->begin = raw[1] & 0x80 ? 1 : 0;
+        ctrl->end = raw[1] & 0x40 ? 1 : 0;
+
+        ctrl->slice_nr = 0;
+        ctrl->slice_nr = *((uint32_t *) (raw));
+        ctrl->slice_nr = ctrl->slice_nr & htobe32(0x003fffff);
+        ctrl->slice_nr = be32toh(ctrl->slice_nr);
+
+        ctrl->session_id = *((uint32_t *) (raw + 4));
+        ctrl->session_id = be32toh(ctrl->session_id);
+}
+
 void forget_session(uint32_t id)
 {
         pthread_mutex_trylock(&collector_mtx); //signal handler should not be blocked
@@ -110,40 +112,40 @@ void forget_session(uint32_t id)
         pthread_mutex_unlock(&collector_mtx);
 }
 
-int get_tlv(struct tlv* t,  uint8* raw)
+int get_tlv(struct tlv* t,  const uint8_t* raw)
 {
-        t->type = raw[0];
+        t->type = (tlv_type) raw[0];
         switch(raw[0]) {
         case TLV_END:
-                return 0;
+                return 2;
                 break;
 
         case TLV_ID:
                 t->len = raw[1];
-                if(raw[2 + t->len] != '\0' || t->len > ID_LEN || t->len < 2)
+                if(raw[1 + t->len] != '\0' || t->len > ID_LEN || t->len < 2)
                         return - 1;
 
-                t->v.id = string(raw[3], t->len - 2);
+                t->data = string((const char *)(raw+2), t->len - 1);
 
                 return t->len + 2;
                 break;
 
         case TLV_NAME:
                 t->len = raw[1];
-                if(raw[2 + t->len] != '\0' || t->len > NAME_LEN || t->len < 2)
+                if(raw[1 + t->len] != '\0' || t->len > NAME_LEN || t->len < 2)
                         return - 1;
 
-                t->v.name = string(raw[3], t->len - 2);
+                t->data = string((const char *)(raw+2), t->len - 1);
 
                 return t->len + 2;
                 break;
 
         case TLV_FACULTY:
                 t->len = raw[1];
-                if(raw[2 + t->len] != '\0' || t->len > FACULTY_LEN || t->len < 2)
+                if(raw[1 + t->len] != '\0' || t->len > FACULTY_LEN || t->len < 2)
                         return - 1;
 
-                t->v.faculty = string(raw[3], t->len - 2);
+                t->data = string((const char *)(raw+2), t->len - 1);
 
                 return t->len + 2;
                 break;
@@ -154,13 +156,66 @@ int get_tlv(struct tlv* t,  uint8* raw)
         }
 
 }
+
+int construct_session(uint32_t id, struct session* s, session_type type)
+{
+        struct slice_set& slc_set = session_map[id];
+        uint8_t curr_state = 0;    //0, 1, 2 : id, name, faculty
+
+        s->session_id = id;
+        s->type = type;
+
+        sort(slc_set.slices.begin(), slc_set.slices.end(), cmp_slice);
+        if(type == SESSION_ADD) {
+                curr_state = 0;
+                struct student_info stu_info;
+
+                for(auto iter1 = slc_set.slices.begin(); iter1 != slc_set.slices.end(); ++iter1) {
+                        for(auto iter2 = iter1->tlvs.begin(); iter2 != iter1->tlvs.end(); ++iter2) {
+                                if(0 == curr_state) {
+                                        if(iter2->type != TLV_ID)
+                                                return -1;
+                                        stu_info.id = iter2->data;
+                                } else if(1 == curr_state) {
+                                        if(iter2->type != TLV_NAME)
+                                                return -1;
+                                        stu_info.name = iter2->data;
+                                } else if(2 == curr_state) {
+                                        if(iter2->type != TLV_FACULTY)
+                                                return -1;
+                                        stu_info.faculty = iter2->data;
+
+                                        s->info_list.push_back(stu_info);
+                                }
+
+                                curr_state = (curr_state + 1) % 3;
+
+                        }
+                }
+        } else if(type == SESSION_DEL) {
+                struct student_info stu_info;
+
+                for(auto iter1 = slc_set.slices.begin(); iter1 != slc_set.slices.end(); ++iter1) {
+                        for(auto iter2 = iter1->tlvs.begin(); iter2 != iter1->tlvs.end(); ++iter2) {
+                                if(iter2->type != TLV_ID)
+                                        return -1;
+                                stu_info.id = iter2->data;
+
+                                s->info_list.push_back(stu_info);
+                        }
+                }
+
+        }
+
+        return 0;
+}
+
 int process_dgram(const uint8_t* raw, int len)
 {
         const uint8_t* curr = raw;
         int remain = len;
         struct control_code cntl_cd;
         struct slice slc;
-        struct slice_set slc_set;
         uint8_t err_flag, end_flag;
 
 
@@ -173,11 +228,10 @@ int process_dgram(const uint8_t* raw, int len)
         //if meet a new session, contruct a bed for it
         pthread_mutex_lock(&collector_mtx);
 
-        if(slices_statistics.find(cntl_cd.session_id) == slices_statistics.end()) {
+        if(session_map.find(cntl_cd.session_id) == session_map.end()) {
 
                 struct slice_set sset;
 
-                sset.current = 0;
                 sset.total = 0;
                 session_map[cntl_cd.session_id] = sset;
 
@@ -223,19 +277,26 @@ int process_dgram(const uint8_t* raw, int len)
 
         pthread_mutex_lock(&collector_mtx);
 
-        &slc_set = session_map[cntl_cd.session_id];
+        struct slice_set& slc_set = session_map[cntl_cd.session_id];
         if(cntl_cd.end)
-                slc_set.total = cntl_cd.slice_nr;
+                slc_set.total = cntl_cd.slice_nr + 1;
 
+        slc_set.slices.push_back(slc);
         if((slc_set.total != 0) &&  (slc_set.slices.size() == slc_set.total)) {
-                struct session con;
+                struct session s;
 
-                con.session_id = session_id
-                
+                if(construct_session(cntl_cd.session_id, &s, cntl_cd.type) == -1) {
+                        forget_session(cntl_cd.session_id);
+                        return -1;
+                }
+//CALL CFB                process_session(&s);
+                forget_session(cntl_cd.session_id);
+
         }
 
         pthread_mutex_unlock(&collector_mtx);
 
+        return 0;
 }
 
 int is_interesting(const ethhdr* eth_hdr)
@@ -278,7 +339,7 @@ int collect_loop()
                 eth_hdr = (struct ethhdr *) buffer;
                 if(is_interesting(eth_hdr)) {
 
-                        if( -1 = process_dgram(buffer + sizeof(struct ethhdr), n - sizeof(struct ethhdr)) )
+                        if( -1 == process_dgram(buffer + sizeof(struct ethhdr), n - sizeof(struct ethhdr)) )
                                 send_rjt();
                 }
         }
@@ -305,8 +366,14 @@ int main()
         configure.dest_macs[0][5] = 0x35;
         configure.list_len++;
 
-        uint8_t test_dgram[8]= {0x01,0xc0,0,0,0,0,0,0x01};
-        process_dgram(test_dgram);
+        uint8_t test_dgram[BUFFER_SIZE];
+        char c;
+        size_t len;
+        while(EOF != (c = getchar()))
+        {
+                test_dgram[len++] = c;
+        }
+        process_dgram(test_dgram+sizeof(struct ethhdr), len);
 
         return 0;
 }
