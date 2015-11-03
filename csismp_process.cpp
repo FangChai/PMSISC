309225a1a091cdf4a39952f4157a10f91fd5f769
Fang Chai <fangbai0330@hotmail.com>
2015-11-03 13:30:32 +0800

    now successfully compiled

--- csismp_process.cpp
+++ csismp_process.cpp
@@ -6,8 +6,10 @@ extern "C"{
 #include <vector>
 #include <iterator>
 #include <algorithm>
+#include <fstream>
 #include <ctime>
 #include <csignal>
+#include <deque>
 #include "conversation.h"
 #include "csismp_limits.h"
 
@@ -15,25 +17,27 @@ extern "C"{
 using namespace std;
 
 
-vector<student_info> local_data;
-vector<student_info> sync_data;
+deque<student_info> local_data;
+deque<student_info> sync_data;
 pthread_mutex_t local_data_mutex;
+
+void print_all_students();
 void process_conversation(conversation *conv)
 {
     switch(conv->type){
         case conversation_type::CONVERSATION_ADD:
             {
                 pthread_mutex_lock(&local_data_mutex);
-                copy(conv->info_list.begin(),conv->info_list.end(),back_inserter(local_data));
-                stable_sort(local_data.rbegin(),local_data.rend(),[](student_info &a,student_info &b)
+                copy(conv->info_list.begin(),conv->info_list.end(),front_inserter(local_data));
+                stable_sort(local_data.begin(),local_data.end(),[](const student_info &a,const student_info &b)
                         {
                             return a.id<b.id || a.id==b.id;
                         });
-                auto new_end=unique(local_data.rbegin(),local_data.rend(),[] (student_info &a,student_info &b)
+                auto new_end=unique(local_data.begin(),local_data.end(),[] (const student_info &a,const student_info &b)
                         {
                             return a.id==b.id;
                         });
-                local_data.erase(new_end,local_data.rend());
+                local_data.erase(new_end,local_data.end());
                 pthread_mutex_unlock(&local_data_mutex);
             }
             print_all_students();
@@ -41,7 +45,7 @@ void process_conversation(conversation *conv)
         case conversation_type::CONVERSATION_DEL:
             {
                  pthread_mutex_lock(&local_data_mutex);
-                 for_each(conv->info_list.begin(),conv->info_list.end(),[](student_info &info)
+                 for_each(conv->info_list.begin(),conv->info_list.end(),[](const student_info &info)
                     {
                         bool success=false;
                         for(auto iter=local_data.begin();iter!=local_data.end();++iter){
@@ -59,7 +63,7 @@ void process_conversation(conversation *conv)
                         //TODO
                         //Construct &Send ACK MSG.
                         }
-                    })
+                    });
             }
             print_all_students();
             break;
@@ -69,16 +73,16 @@ void process_conversation(conversation *conv)
             break;
         case conversation_type::CONVERSATION_SYN:
             {
-                copy(conv->info_list.begin(),conv->info_list.end();back_inserter(sync_data));
-                stable_sort(sync_data.rbegin(),sync_data.rend(),[](student_info &a,student_info &b)
+                copy(conv->info_list.begin(),conv->info_list.end(),front_inserter(sync_data));
+                stable_sort(sync_data.begin(),sync_data.end(),[](const student_info &a,const student_info &b)
                         {
                             return a.id<b.id || a.id==b.id;
                         });
-                auto new_end=unique(sync_data.rbegin(),sync_data.rend(),[] (student_info &a,student_info &b)
+                auto new_end=unique(sync_data.begin(),sync_data.end(),[] (const student_info &a,const student_info &b)
                         {
                             return a.id==b.id;
                         });
-                sync_data.erase(new_end,sync_data.rend());
+                sync_data.erase(new_end,sync_data.end());
             }
             break;
         default:
@@ -96,7 +100,7 @@ void *on_timer_up()
     conversation session;
     session.type=conversation_type::CONVERSATION_SYN;
     pthread_mutex_lock(&local_data_mutex);
-    session.info_list.assign(local_data);
+    session.info_list.assign(local_data.begin(),local_data.end());
     pthread_mutex_unlock(&local_data_mutex);
     //TODO
     //Send MSG.
